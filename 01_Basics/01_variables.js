3b3b59d053c2705f0644dfad6e29fd21a605e987
Shruti Kale <118712088+kshruti3030@users.noreply.github.com>
2026-04-25 08:24:41 +0000

    Variables & dataTYpes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144556
+let accountEmail = "shruti33@google.com"
+var accountPassword ="12345"
+accountCity ="Pune"
+let accountState;
+
+// accountId =2
+// prefer do not use var bcz  of ussue in block scope and functional scope
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountCity, accountState])
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(Hello)/ we are using nodejs not browser
+
+console.log(3+3)
+
+
+//*primitive Data types*
+//number => 2 to power 53
+//bigint
+//string => ""
+// boolean => true/false
+//null =>standalone value
+//undefined
+//symbol=> unique
+
+
+//*Objects*
+
+console.log(typeof "Shruti");
+console.log(typeof null);
+console.log(typeof undefined);
+
+
