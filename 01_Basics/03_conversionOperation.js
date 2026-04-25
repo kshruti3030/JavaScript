570e69f84f4ae26e9407c43dbed786308bf64123
Shruti Kale <118712088+kshruti3030@users.noreply.github.com>
2026-04-25 09:04:17 +0000

    Conversion of Data type

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,40 @@
+let score = "Shrui"
+let score1 ="33"
+
+// 2 type ne type check karu shkto aapn
+
+console.log(typeof score);
+console.log(typeof(score));
+
+//String to int conversion
+let valueNumber = Number(score)
+let ExNumber = Number(score1)
+
+console.log(typeof valueNumber);
+console.log(typeof(valueNumber));
+console.log(valueNumber); //op->  NaN [Not An Nubmer] karan score he as string ahe
+console.log(typeof ExNumber);
+
+//"33" => 33
+//"33abc" => NaN
+//true=>1; false => 0
+
+let isLoggedIn ="Shruti"
+
+let boolenIsLoggedIn =Boolean(isLoggedIn)
+
+// console.log(boolenIsLoggedIn);
+
+
+let someNum ="";
+let boolenIsLoggedInNum = Boolean(someNum)
+console.log(boolenIsLoggedIn);
+console.log(someNum);
+
+
+
+
+
+
+
+
